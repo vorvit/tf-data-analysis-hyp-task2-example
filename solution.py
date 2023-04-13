59b0519d6ffcc7175652a54720eba7bc15daf7ee
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-13 20:11:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    res = stats.cramervonmises_2samp(x, y)
+    return res.pvalue < 0.01
