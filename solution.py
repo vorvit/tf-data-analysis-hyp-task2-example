7a76b0a2e193e3ed76eaba252b9a5b959642d4e1
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-14 10:51:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from hyppo.ksample import MMD
 
 chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     
-    res = stats.anderson_ksamp([x, y])
+    res = MMD(compute_kernel = "rbf", gamma = 1).test(x, y)[1]
+#   res = stats.anderson_ksamp([x, y])
 #   res = stats.cramervonmises_2samp(x, y)
-    return res.pvalue < 0.01
+#   return res.pvalue < 0.01
+    return res < 0.01
